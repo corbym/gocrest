2ada973576a3a8ba9b976057264af0c7a58f0f58
mattcorby-eaglen <matt.corby-eaglen@sedex.com>
2023-04-05 16:54:04 +0100

    attempt to fix/lessen some problems mentioned in issue #9

--- is/greaterthanorequalto.go
+++ is/greaterthanorequalto.go
@@ -4,7 +4,7 @@ import "github.com/corbym/gocrest"
 
 // GreaterThanOrEqualTo is a shorthand matcher for anyOf(greaterThan(x), equalTo(x))
 // Returns a matcher matching if actual >= expected (using deepEquals).
-func GreaterThanOrEqualTo[A int | int8 | int16 | int32 | int64 | float32 | float64 | uint | uint16 | uint32 | uint64 | string](expected A) *gocrest.Matcher[A] {
+func GreaterThanOrEqualTo[A Comparable](expected A) *gocrest.Matcher[A] {
 	matcher := new(gocrest.Matcher[A])
 	matcher.Matches = func(actual A) bool {
 		anyOf := AnyOf(GreaterThan(expected), EqualTo(expected))
--- is/nil.go
+++ is/nil.go
@@ -3,23 +3,44 @@ package is
 import (
 	"fmt"
 	"github.com/corbym/gocrest"
-	"reflect"
 )
 
+var description = "value that is <nil>"
+
 // Nil matches if the actual value is nil
-func Nil[A any]() *gocrest.Matcher[A] {
-	match := new(gocrest.Matcher[A])
-	match.Describe = "value that is <nil>"
-	match.Matches = func(actual A) bool {
+func Nil() *gocrest.Matcher[error] {
+	match := new(gocrest.Matcher[error])
+	match.Describe = description
+	match.Matches = func(actual error) bool {
 		match.Actual = fmt.Sprintf("%v", actual)
-		if any(actual) == nil {
-			return true
-		}
-		switch reflect.TypeOf(actual).Kind() {
-		case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
-			return reflect.ValueOf(actual).IsNil()
-		}
-		return false // anything else is never nil (hopefully)
+		return actual == nil
+	}
+	return match
+}
+func NilArray[A any]() *gocrest.Matcher[[]A] {
+	match := new(gocrest.Matcher[[]A])
+	match.Describe = description
+	match.Matches = func(actual []A) bool {
+		match.Actual = fmt.Sprintf("%v", actual)
+		return actual == nil
+	}
+	return match
+}
+func NilMap[K comparable, V any]() *gocrest.Matcher[map[K]V] {
+	match := new(gocrest.Matcher[map[K]V])
+	match.Describe = description
+	match.Matches = func(actual map[K]V) bool {
+		match.Actual = fmt.Sprintf("%v", actual)
+		return actual == nil
+	}
+	return match
+}
+func NilPtr[T any]() *gocrest.Matcher[*T] {
+	match := new(gocrest.Matcher[*T])
+	match.Describe = description
+	match.Matches = func(actual *T) bool {
+		match.Actual = fmt.Sprintf("%v", actual)
+		return actual == nil
 	}
 	return match
 }
