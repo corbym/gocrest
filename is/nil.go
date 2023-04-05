4c0d6d6243145fd3798e5012d6466ebcc91398a3
mattcorby-eaglen <matt.corby-eaglen@sedex.com>
2023-04-05 17:07:36 +0100

    godoc added

--- is/nil.go
+++ is/nil.go
@@ -7,7 +7,7 @@ import (
 
 var description = "value that is <nil>"
 
-// Nil matches if the actual value is nil
+// Nil matches if the actual error value is nil
 func Nil() *gocrest.Matcher[error] {
 	match := new(gocrest.Matcher[error])
 	match.Describe = description
@@ -17,6 +17,8 @@ func Nil() *gocrest.Matcher[error] {
 	}
 	return match
 }
+
+// NilArray matches if the actual array value is nil
 func NilArray[A any]() *gocrest.Matcher[[]A] {
 	match := new(gocrest.Matcher[[]A])
 	match.Describe = description
@@ -26,6 +28,8 @@ func NilArray[A any]() *gocrest.Matcher[[]A] {
 	}
 	return match
 }
+
+// NilMap matches if the actual map value is nil
 func NilMap[K comparable, V any]() *gocrest.Matcher[map[K]V] {
 	match := new(gocrest.Matcher[map[K]V])
 	match.Describe = description
@@ -35,6 +39,8 @@ func NilMap[K comparable, V any]() *gocrest.Matcher[map[K]V] {
 	}
 	return match
 }
+
+// NilPtr matches if the actual pointer to T is nil
 func NilPtr[T any]() *gocrest.Matcher[*T] {
 	match := new(gocrest.Matcher[*T])
 	match.Describe = description
