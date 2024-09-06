a0129432961ca8a1e7fd867eec130b522fd21d8a
mattcorby-eaglen <matt.corby-eaglen@sedex.com>
2024-09-06 14:32:36 +0100

    * Correct documentation * remove redundant code

--- is/nil.go
+++ is/nil.go
@@ -1,7 +1,6 @@
 package is
 
 import (
-	"fmt"
 	"github.com/corbym/gocrest"
 )
 
@@ -12,7 +11,6 @@ func Nil() *gocrest.Matcher[error] {
 	match := new(gocrest.Matcher[error])
 	match.Describe = description
 	match.Matches = func(actual error) bool {
-		match.Actual = fmt.Sprintf("%v", actual)
 		return actual == nil
 	}
 	return match
@@ -23,7 +21,6 @@ func NilArray[A any]() *gocrest.Matcher[[]A] {
 	match := new(gocrest.Matcher[[]A])
 	match.Describe = description
 	match.Matches = func(actual []A) bool {
-		match.Actual = fmt.Sprintf("%v", actual)
 		return actual == nil
 	}
 	return match
@@ -34,7 +31,6 @@ func NilMap[K comparable, V any]() *gocrest.Matcher[map[K]V] {
 	match := new(gocrest.Matcher[map[K]V])
 	match.Describe = description
 	match.Matches = func(actual map[K]V) bool {
-		match.Actual = fmt.Sprintf("%v", actual)
 		return actual == nil
 	}
 	return match
@@ -45,7 +41,6 @@ func NilPtr[T any]() *gocrest.Matcher[*T] {
 	match := new(gocrest.Matcher[*T])
 	match.Describe = description
 	match.Matches = func(actual *T) bool {
-		match.Actual = fmt.Sprintf("%v", actual)
 		return actual == nil
 	}
 	return match
